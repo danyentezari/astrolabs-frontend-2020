732649922c039da8814b3cee8c44249e707a687e
danyentezari <dany.entezari@gmail.com>
2020-12-12 13:45:28 +0400

    Added NavBar and Button components.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand">Navbar</a>
+        <div className="d-flex">
+          <input className="form-control me-2" type="search" 
+          placeholder="Search" aria-label="Search" />
+
+          <button className="btn btn-outline-success" 
+          type="submit">Search</button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+function Button() {
+  return(
+    <button type="button" className="btn btn-success">Success</button>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand">Navbar</a>
-          <div className="d-flex">
-            <input className="form-control me-2" type="search" 
-            placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" 
-            type="submit">Search</button>
-          </div>
-        </div>
-      </nav>
-
+      <NavBar />
+      <Button />
     </div>
   );
 }
