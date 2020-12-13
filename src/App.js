7d0e9064d78da837a738882912f94abf3f86263e
danyentezari <dany.entezari@gmail.com>
2020-12-13 11:48:20 +0400

    Example with more components.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function NavBar() {
+function NavBar(prop) {
   return (
     <nav className="navbar navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand">Navbar</a>
-        <div className="d-flex">
-          <input className="form-control me-2" type="search" 
-          placeholder="Search" aria-label="Search" />
 
-          <button className="btn btn-outline-success" 
-          type="submit">Search</button>
+        <div className="d-flex flex-grow-1">
+          {prop.children}
         </div>
+        
+        {prop.extraComponent}
       </div>
     </nav>
   )
@@ -29,7 +28,38 @@ function App() {
   return (
     <div className="App">
   
-      <Button>Save</Button>
+      <NavBar extraComponent={
+        <div className="d-flex">
+          <div>
+            <input className="form-control me-2" type="search" 
+            placeholder="Search" aria-label="Search" />
+
+            <button className="btn btn-outline-success" 
+            type="submit">Search</button>
+          </div>
+          </div>
+      }>
+        <Button>Home</Button>
+        <Button>About</Button>
+        <Button>Contact</Button>
+      </NavBar>
+
+      <div style={{'height': 'calc(100vh - 160px)'}}></div>
+
+      <NavBar>
+        Legal
+        <ul>
+          <li><a href="">Terms &amp; Conditions</a></li>
+          <li><a href="">Privacy Policy</a></li>
+          <li><a href="">Return Policy</a></li>
+        </ul>
+
+        About
+        <ul>
+          <li><a href="">Our Company</a></li>
+          <li><a href="">Our History</a></li>
+        </ul>
+      </NavBar>
   
     </div>
   );
