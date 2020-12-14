c3ae0036c342d27ecbc1c325218267dec19e4693
danyentezari <dany.entezari@gmail.com>
2020-12-14 19:32:10 +0400

    Updated components and CSS.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
-function NavBar(prop) {
+function NavBar(props) {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className={`navbar navbar-dark bg-dark ${props.className}`}>
       <div className="container-fluid">
+
         <a className="navbar-brand">Navbar</a>
 
         <div className="d-flex flex-grow-1">
-          {prop.children}
+          {props.children}
         </div>
         
-        {prop.extraComponent}
+        {props.extraComponent}
       </div>
     </nav>
   )
 }
 
 
-function Button(prop) {
+function Button(props) {
   return(
-  <button type="button" className="btn btn-primary">{prop.children}</button>
+  <button type="button" className={props.className}>{props.children}</button>
   )
 }
 
 function App() {
   return (
     <div className="App">
-  
+
       <NavBar extraComponent={
-        <div className="d-flex">
-          <div>
-            <input className="form-control me-2" type="search" 
-            placeholder="Search" aria-label="Search" />
+          <div className="d-flex">
+              <input className="form-control me-2" type="search" 
+              placeholder="Search" aria-label="Search" />
 
-            <button className="btn btn-outline-success" 
-            type="submit">Search</button>
+              <button className="btn btn-outline-success" 
+              type="submit">Search</button>
           </div>
-          </div>
-      }>
-        <Button>Home</Button>
-        <Button>About</Button>
-        <Button>Contact</Button>
+        }>
+        <Button className="btn btn-primary">Home</Button>
+        <Button className="btn btn-primary">About</Button>
+        <Button className="btn btn-primary">Contact</Button>
       </NavBar>
 
-      <div style={{'height': 'calc(100vh - 160px)'}}></div>
+      <NavBar className="fixed-bottom"/>
 
-      <NavBar>
-        Legal
-        <ul>
-          <li><a href="">Terms &amp; Conditions</a></li>
-          <li><a href="">Privacy Policy</a></li>
-          <li><a href="">Return Policy</a></li>
-        </ul>
-
-        About
-        <ul>
-          <li><a href="">Our Company</a></li>
-          <li><a href="">Our History</a></li>
-        </ul>
-      </NavBar>
-  
     </div>
   );
 }
-
-
-
 
 export default App;
